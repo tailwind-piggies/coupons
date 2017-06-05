de084fe655723ed5ba906fec78690fd3be0c2ee7
seasonli <season.chopsticks@gmail.com>
2017-06-05 20:18:09 +0800

    feat(aliyuntoken)

--- actions/Util.js
+++ actions/Util.js
@@ -1,4 +1,7 @@
+const moment = require('moment')
 const ALY = require('aliyun-sdk')
+
+const crypto = require('crypto')
 
 const sts = new ALY.STS({
   accessKeyId: 'LTAIQXU5hVE8CKy3',
@@ -8,7 +11,7 @@ const sts = new ALY.STS({
 })
 
 module.exports = function(router) {
-  router.get('/utils/aliyun/sts/token', async ctx => {
+  router.get('/utils/aliyun/uploadtoken', async ctx => {
     ctx.body = await new Promise(resolve => {
       sts.assumeRole({
         Action: 'AssumeRole',
@@ -17,10 +20,29 @@ module.exports = function(router) {
         RoleSessionName: 'RoleSessionName'
       }, function (err, res) {
         if (!err) {
+          const date = `${moment().utc().format('ddd, DD MMM YYYY hh:mm:ss')} GMT`
+          const filename = new Buffer(date).toString('base64')
+          const fileType = ctx.request.query.type || ''
+          const aks = res.Credentials.AccessKeySecret
+          const akid = res.Credentials.AccessKeyId
+          const st = res.Credentials.SecurityToken
+          const args =
+`PUT
+
+${fileType}
+${date}
+x-oss-security-token:${st}
+/planetoid/${filename}`
+          const sign = new Buffer(crypto.createHmac('sha1', aks).update(args).digest()).toString('base64')
+
           resolve({
             success: true,
             data: {
-              token: res
+              token: res,
+              authorization: `OSS ${akid}:${sign}`,
+              date,
+              filename,
+              fileType
             }
           })
         } else {
