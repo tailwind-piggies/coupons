1c4ac405c33de8d62f61e759cb7a7649c694f792
seasonli <season.chopsticks@gmail.com>
2017-06-05 20:58:36 +0800

    feat(aliyuntoken)

--- actions/Util.js
+++ actions/Util.js
@@ -20,29 +20,22 @@ module.exports = function(router) {
         RoleSessionName: 'RoleSessionName'
       }, function (err, res) {
         if (!err) {
-          const date = `${moment().utc().format('ddd, DD MMM YYYY hh:mm:ss')} GMT`
-          const filename = new Buffer(date).toString('base64')
-          const fileType = ctx.request.query.type || ''
           const aks = res.Credentials.AccessKeySecret
-          const akid = res.Credentials.AccessKeyId
-          const st = res.Credentials.SecurityToken
-          const args =
-`PUT
-
-${fileType}
-${date}
-x-oss-security-token:${st}
-/planetoid/${filename}`
-          const sign = new Buffer(crypto.createHmac('sha1', aks).update(args).digest()).toString('base64')
+          const policy = new Buffer(JSON.stringify({
+            expiration: moment(moment().valueOf() + 15 * 60 * 1000),
+            conditions: [{
+              bucket: 'planetoid'
+            }]
+          })).toString('base64')
+          const signature = new Buffer(crypto.createHmac('sha1', aks).update(policy).digest()).toString('base64')
 
           resolve({
             success: true,
             data: {
-              token: res,
-              authorization: `OSS ${akid}:${sign}`,
-              date,
-              filename,
-              fileType
+              akId: res.Credentials.AccessKeyId,
+              token: res.Credentials.SecurityToken,
+              policy,
+              signature
             }
           })
         } else {
